c84de291e06fdeb7cb00cb838b40fd49e010ad55
Surya Singh <psurya.924@gmail.com>
2020-05-26 18:48:46 -0500

    Add files via upload

---
+++ GTA V self driving/history.py
@@ -0,0 +1,104 @@
+
+## ---(Sun May 10 16:00:37 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('D:/GameProg/models/research/object_detection/utils/label_map_util.py', wdir='D:/GameProg/models/research/object_detection/utils')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/.spyder-py3/temp.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/temp.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/.spyder-py3/train_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/train_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/train_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('C:/Users/Surya/.spyder-py3/test_model.py', wdir='C:/Users/Surya/.spyder-py3')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+
+## ---(Mon May 11 19:42:31 2020)---
+runfile('C:/Users/Surya/Desktop/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Desktop/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Desktop/Tello-Python/Tello_Video/tello.py', wdir='C:/Users/Surya/Desktop/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Desktop/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Desktop/Tello-Python/Tello_Video')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+
+## ---(Mon May 11 20:07:42 2020)---
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Mon May 11 20:28:27 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+
+## ---(Mon May 11 20:51:40 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+
+## ---(Mon May 11 20:52:23 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Mon May 11 21:36:55 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Tue May 12 15:59:39 2020)---
+runfile('D:/tello-openpose/tello_openpose.py', wdir='D:/tello-openpose')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Tue May 12 17:16:29 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Tue May 12 17:52:54 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Tue May 12 18:35:46 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Sun May 17 20:53:49 2020)---
+runfile('D:/GameProg/models/research/object_detection/object_detection_tutorial.py', wdir='D:/GameProg/models/research/object_detection')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/main.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+
+## ---(Tue May 26 16:29:23 2020)---
+runfile('C:/Users/Surya/Downloads/Forest-Fire-Prediction-Website-master/app.py', wdir='C:/Users/Surya/Downloads/Forest-Fire-Prediction-Website-master')
+runfile('C:/Users/Surya/Downloads/Forest-Fire-Prediction-Website-master/forest_fire.py', wdir='C:/Users/Surya/Downloads/Forest-Fire-Prediction-Website-master')
+
+## ---(Tue May 26 17:40:54 2020)---
+runfile('C:/Users/Surya/Tello-Python/Tello_Video/untitled0.py', wdir='C:/Users/Surya/Tello-Python/Tello_Video')
+runfile('D:/flaskapp.py', wdir='D:')
+
+## ---(Tue May 26 18:07:03 2020)---
+runfile('D:/flaskapp.py', wdir='D:')
+
+## ---(Tue May 26 18:11:03 2020)---
+runfile('D:/flaskapp.py', wdir='D:')
